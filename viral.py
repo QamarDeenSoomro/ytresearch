cfcaedda1c7d4fd9b3090e11ef08edf866a57567
Qamar Deen Soomro <102368809+QamarDeenSoomro@users.noreply.github.com>
2025-07-22 08:59:26 +0500

    Update viral.py

--- viral.py
+++ viral.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "AIzaSyA7JlDRXPIynbudN-ensSD0I59vBnkbOts"
+API_KEY = "AIzaSyBSskhuPie--IC5apebIRpD3xwCk0lrJ4g"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
